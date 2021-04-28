5b3fdc13e91e9ff18e2ec54482b5b6c75f766614
Jason Rivera <jp_rivera_@hotmail.com>
2021-04-28 13:44:16 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Header from './Header.js';
 import Searchbar from './Searchbar.js';
+import Button from './Button.js';
 
 const App = () => {
     return (
         <div className="App">
-            App component
             <Header />
             <Searchbar />
+            <div className="button-container">
+                <Button name="Google Search" url="https://www.google.com"/>
+                <Button name="I'm Feeling Lucky" url="https://www.google.com/doodles"/>
+            </div>
         </div>
     )
 }
---
+++ src/Button.js
@@ -0,0 +1,11 @@
+import './Button.css';
+
+const Button = (props) => {
+    return (
+        <div>
+            <a href={props.url}><button type="button" className="btn">{props.name}</button></a>
+        </div>
+    )
+}
+
+export default Button;
